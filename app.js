1840a7d3b8f5e91e39b7823a71734c94dc23e544
strawberry-flavored-milk <hikyj1701@gmail.com>
2021-08-23 23:09:55 +0900

    Added Loading a Saved Username System

--- app.js
+++ app.js
@@ -3,15 +3,30 @@ const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
 
 const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 
 function onLoginSubmit(event) {
   event.preventDefault();
   loginForm.classList.add(HIDDEN_CLASSNAME);
   const username = loginInput.value;
-  localStorage.setItem("username", username);
-  greeting.innerText = `Hello ${username} keep going`;
-  // greeting.innerText = "Hello " + username; --> another way for combining strings
+  localStorage.setItem(USERNAME_KEY, username);
+  greetings(username);
+}
+
+function greetings(username) {
+  greeting.innerText = `Hello ${username}`;
+  // greeting.innerText = "Hello " + username;
+  // --> another way for combining strings
   greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
-loginForm.addEventListener("submit", onLoginSubmit);
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (savedUsername === null) {
+  //show the form
+  loginForm.addEventListener("submit", onLoginSubmit);
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+} else {
+  //show the greetings
+  greetings(savedUsername);
+}

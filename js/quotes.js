d58b655f9b03a983fe41c7daf664da046716daf7
strawberry-flavored-milk <hikyj1701@gmail.com>
2021-08-26 00:00:37 +0900

    Added Quotes Section

---
+++ js/quotes.js
@@ -0,0 +1,92 @@
+const quotes = [
+  {
+    quote:
+      "You might be winning but you're not growing, and growing might be the most important form of winning.",
+    author: "Jordan B. Peterson",
+  },
+  {
+    quote:
+      "Compare yourself to who you were yesterday, not to who someone else is today.",
+    author: "Jordan B. Peterson",
+  },
+  {
+    quote:
+      "I can accept failure, everyone fails at something. But I can't accept not trying.",
+    author: "Michael Jordan",
+  },
+  {
+    quote: "Turn your wounds into wisdom.",
+    author: "Oprah Winfrey",
+  },
+  {
+    quote: "Learn the rules like a pro, so you can break them like an artist.",
+    author: "Pablo Picasso",
+  },
+  {
+    quote:
+      "Remember that no matter how cool you think you may be, you're not cool enough to look down on anyone...ever.",
+    author: "Paul Walker",
+  },
+  {
+    quote:
+      "Our lives begin to end the day we become silent about things that matter.",
+    author: "Martin Luther King",
+  },
+  {
+    quote:
+      "Everyone thinks of changing the world, but no one thinks of changing himself.",
+    author: "Lev Tolstoy",
+  },
+  {
+    quote: "Strive not to be a success, but rather to be of value.",
+    author: "Albert Einstein",
+  },
+  {
+    quote:
+      "All our dreams can come true, if we have the courage to pursue them.",
+    author: "Walt Disney",
+  },
+  {
+    quote:
+      "One must live the way on thinks or end up thinking the way one has lived.",
+    author: "Paul Bourget",
+  },
+  {
+    quote: "불가사의한 승리는 있지만 불가사의한 패배는 없다.",
+    author: "Katsuya Nomura",
+  },
+  {
+    quote:
+      "Life is a little like wrestling a gorilla. You don't quit when you're tired-you quit when the gorilla is tired.",
+    author: "Robert Strauss",
+  },
+  {
+    quote:
+      "Twenty years from now you will be more disappointed by the things you didn't do than by the ones you did. So throw off the bowlines. Sail away from the safe harbor. Catch the trade winds in your sails. Explore. Dream. Discover.",
+    author: "Mark Twain",
+  },
+  {
+    quote:
+      "가볍게 움직이지 말라. 침착하게 태산같이 무거이 행동하라. 나를 알고 적을 알아야만 백 번 싸워도 위태함이 없다.",
+    author: "이순신",
+  },
+  {
+    quote:
+      "모든 일에 있어서 시간이 부족하지 않을까를 염려하지 말고 다만 내가 마음을 바쳐 최선을 다할 수 있을지 그것을 걱정하라.",
+    author: "정조",
+  },
+  {
+    quote:
+      "내 사랑하는 형제들아 너희가 알거니와 사람마다 듣기는 속히 하고 말하기는 더디 하며 성내기도 더디 하라 사람의 성내는 것이 하나님의 의를 이루지 못함이니라",
+    author: "야고보서 1:19-20",
+  },
+  {
+    quote:
+      "두려워하지 말라 내가 너와 함께 함이라 놀라지 말라 나는 네 하나님이 됨이라 내가 너를 굳세게 하리라 참으로 너를 도와 주리라 참으로 나의 의로운 오른손으로 너를 붙들리라",
+    author: "이사야 41:10",
+  },
+  {
+    quote: "사랑엔 거짓이 없나니 악을 미워하고 선에 속하라",
+    author: "로마서 12:9",
+  },
+];
